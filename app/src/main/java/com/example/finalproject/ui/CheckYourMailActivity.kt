51429f1ee2d6dd6a1a8279ccf90e2cc0e21a01e7
moelkady27 <mohamedkhaledelkady11@gmail.com>
2023-12-05 08:39:38 +0200

    tool bar

--- app/src/main/java/com/example/finalproject/ui/CheckYourMailActivity.kt
+++ app/src/main/java/com/example/finalproject/ui/CheckYourMailActivity.kt
@@ -5,15 +5,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.finalproject.R
 import kotlinx.android.synthetic.main.activity_check_your_mail.btn_ok
+import kotlinx.android.synthetic.main.activity_check_your_mail.toolbar_check
 
 class CheckYourMailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_check_your_mail)
 
+        setUpActionBar()
+
         btn_ok.setOnClickListener {
             startActivity(Intent(this@CheckYourMailActivity, VerificationCodeActivity::class.java))
         }
 
+    }
+
+    private fun setUpActionBar() {
+        setSupportActionBar(toolbar_check)
+
+        val actionBar = supportActionBar
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_arrow_back)
+            actionBar.title = ""
+        }
+
+        toolbar_check.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
