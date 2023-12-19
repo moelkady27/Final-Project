5ed27ebc9305e4d35770c07c0c758450ff910f40
moelkady27 <mohamedkhaledelkady11@gmail.com>
2023-12-19 23:38:02 +0200

    finish SignUp

--- app/src/main/java/com/example/finalproject/ui/register/activities/SignUpActivity.kt
+++ app/src/main/java/com/example/finalproject/ui/register/activities/SignUpActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.util.Patterns
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatCheckBox
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.finalproject.R
@@ -85,40 +86,58 @@ class SignUpActivity : BaseActivity() {
 
 //        signUpViewModel.signUp(userName, email, password, confirmPass)
 
-        if (userName.isNotEmpty() && isUserNameValid(userName) && email.isNotEmpty() && isValidEmail(email)
-            && password.isNotEmpty() && isPasswordValid(password) && confirmPass.isNotEmpty()
-            && password == confirmPass) {
-
-            showProgressDialog(this@SignUpActivity , "Signing Up...")
-
-            signUpViewModel.signUp(userName, email, password, confirmPass)
+        if (isValidInput()) {
+            showProgressDialog(this@SignUpActivity, "Signing Up...")
+            signUpViewModel.signUp(userName , email, password, confirmPass)
         }
-        else {
-            if (userName.isEmpty()) {
-                et_username_sign_up.error = "Username is not allowed to be empty."
-            }
-            else if (!isUserNameValid(userName)) {
-                et_username_sign_up.error = "Username must be at least 6 characters long."
-            }
-            if (email.isEmpty()) {
-                et_email_sign_up.error = "Email is not allowed to be empty."
-            }
-            else if (!isValidEmail(email)) {
-                et_email_sign_up.error = "Please enter a valid email address."
-            }
-            if (password.isEmpty()) {
-                et_password_sign_up.error = "Password cannot be empty."
-            }
-            else if (!isPasswordValid(password)) {
-                et_password_sign_up.error = "Password must be at least 8 characters long."
-            }
-            if (confirmPass.isEmpty()) {
-                et_confirm_password_sign_up.error = "Confirm Password cannot be empty."
-            }
-            else if (password != confirmPass) {
-                et_confirm_password_sign_up.error = "Passwords do not match."
-            }
+    }
+
+    private fun isValidInput(): Boolean {
+        val userName = et_username_sign_up.text.toString().trim()
+        val email = et_email_sign_up.text.toString().trim()
+        val password = et_password_sign_up.text.toString().trim()
+        val confirmPass = et_confirm_password_sign_up.text.toString().trim()
+
+        val cbTermsAndConditions = findViewById<AppCompatCheckBox>(R.id.cb_terms_and_conditions)
+
+        if (userName.isEmpty()) {
+            et_username_sign_up.error = "Username is not allowed to be empty."
+            return false
+        } else if (!isUserNameValid(userName)) {
+            et_username_sign_up.error = "Username must be at least 6 characters long."
+            return false
         }
+
+        if (email.isEmpty()) {
+            et_email_sign_up.error = "Email is not allowed to be empty."
+            return false
+        } else if (!isValidEmail(email)) {
+            et_email_sign_up.error = "Please enter a valid email address."
+            return false
+        }
+
+        if (password.isEmpty()) {
+            et_password_sign_up.error = "Password cannot be empty."
+            return false
+        } else if (!isPasswordValid(password)) {
+            et_password_sign_up.error = "Password must be at least 8 characters long."
+            return false
+        }
+
+        if (confirmPass.isEmpty()) {
+            et_confirm_password_sign_up.error = "Confirm Password cannot be empty."
+            return false
+        } else if (password != confirmPass) {
+            et_confirm_password_sign_up.error = "Passwords do not match."
+            return false
+        }
+
+        if (!cbTermsAndConditions.isChecked) {
+            Toast.makeText(this, "Please accept the terms and conditions", Toast.LENGTH_SHORT).show()
+            return false
+        }
+
+        return true
     }
 
     private fun isUserNameValid(userName: CharSequence): Boolean {
