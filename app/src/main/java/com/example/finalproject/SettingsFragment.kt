43875d68a42173d248e6ff647215bd1a0653ff17
AhmedSommat <ahmed.sommat97@gmail.com>
2024-02-24 19:28:36 +0200

    Settings Screen

--- app/src/main/java/com/example/finalproject/SettingsFragment.kt
+++ app/src/main/java/com/example/finalproject/SettingsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.finalproject
 
+import android.graphics.PorterDuff
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_settings.btn_edit_profile
 
 class SettingsFragment : Fragment() {
 
@@ -22,5 +24,15 @@ class SettingsFragment : Fragment() {
             container,
             false
         )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btn_edit_profile.setOnClickListener {
+            btn_edit_profile.setBackgroundColor(resources.getColor(R.color.colorPrimary))
+            btn_edit_profile.setTextColor(resources.getColor(R.color.white))
+            btn_edit_profile.setIconTintResource(R.color.white)
+        }
     }
 }
