4ba441df9c09fcb104a1644990e845ebf80e61f8
AhmedSommat <ahmed.sommat97@gmail.com>
2024-04-02 05:15:50 +0200

    Home UI

--- app/src/main/java/com/example/finalproject/ui/home/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/finalproject/ui/home/fragment/HomeFragment.kt
@@ -5,9 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.finalproject.R
+import com.example.finalproject.ui.home.adapter.HomeFeaturedAdapter
+import com.example.finalproject.ui.home.adapter.HomePopularAdapter
 
 class HomeFragment : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var homePopularAdapter: HomePopularAdapter
+    private lateinit var homeFeaturedAdapter: HomeFeaturedAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,5 +31,24 @@ class HomeFragment : Fragment() {
             container,
             false
         )
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = view.findViewById(R.id.rv_home_popular)
+
+        homePopularAdapter = HomePopularAdapter()
+        recyclerView.layoutManager = LinearLayoutManager(requireContext() ,
+            LinearLayoutManager.HORIZONTAL , false)
+        recyclerView.adapter = homePopularAdapter
+
+        recyclerView = view.findViewById(R.id.rv_home_featured)
+
+        homeFeaturedAdapter = HomeFeaturedAdapter()
+        recyclerView.layoutManager = LinearLayoutManager(requireContext() ,
+            LinearLayoutManager.VERTICAL , false)
+        recyclerView.adapter = homeFeaturedAdapter
+
     }
 }
