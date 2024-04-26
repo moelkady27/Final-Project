af5f947d3bbc81e4cddcddd2929db8ab3dd3b781
AhmedSommat <ahmed.sommat97@gmail.com>
2024-04-26 22:36:44 +0300

    Extra Information Success

--- app/src/main/java/com/example/finalproject/ui/add_listing/activities/ExtraInformationActivity.kt
+++ app/src/main/java/com/example/finalproject/ui/add_listing/activities/ExtraInformationActivity.kt
@@ -2,9 +2,13 @@ package com.example.finalproject.ui.add_listing.activities
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.example.finalproject.R
+import com.google.android.material.bottomsheet.BottomSheetDialog
+import kotlinx.android.synthetic.main.activity_extra_information.btn_submit
 import kotlinx.android.synthetic.main.activity_extra_information.count_balcony
 import kotlinx.android.synthetic.main.activity_extra_information.count_bathroom
 import kotlinx.android.synthetic.main.activity_extra_information.count_bedroom
@@ -15,6 +19,8 @@ import kotlinx.android.synthetic.main.activity_extra_information.plus_balcony
 import kotlinx.android.synthetic.main.activity_extra_information.plus_bathroom
 import kotlinx.android.synthetic.main.activity_extra_information.plus_bedroom
 import kotlinx.android.synthetic.main.activity_extra_information.toolbar_extra_information
+import kotlinx.android.synthetic.main.extra_information_success.add_more_extra_information_success
+import kotlinx.android.synthetic.main.extra_information_success.finish_extra_information_success
 
 class ExtraInformationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -43,6 +49,10 @@ class ExtraInformationActivity : AppCompatActivity() {
 
         minus_balcony.setOnClickListener {
             decreaseCountBalcony()
+        }
+
+        btn_submit.setOnClickListener {
+            showExtraInformationSuccess()
         }
 
         setupActionBar()
@@ -121,6 +131,30 @@ class ExtraInformationActivity : AppCompatActivity() {
         toolbar_extra_information.setNavigationOnClickListener {
             onBackPressed()
         }
+    }
+
+    private fun showExtraInformationSuccess() {
+        val bottomSheetDialog = BottomSheetDialog(this, R.style.BottomSheetDialog)
+
+        val messageDialogView =
+            layoutInflater.inflate(R.layout.extra_information_success, null)
+
+        messageDialogView.background =
+            ContextCompat.getDrawable(this, R.drawable.message_options_dialog_background)
+
+        val addMoreButton = messageDialogView.findViewById<TextView>(R.id.add_more_extra_information_success)
+        val finishButton = messageDialogView.findViewById<TextView>(R.id.finish_extra_information_success)
+
+        addMoreButton.setOnClickListener {
+
+        }
+
+        finishButton.setOnClickListener {
+
+        }
+
+        bottomSheetDialog.setContentView(messageDialogView)
+        bottomSheetDialog.show()
     }
 
 }
