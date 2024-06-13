89eb6a947054f875fec5f4c0c52a43810e95c1c2
AhmedSommat <ahmed.sommat97@gmail.com>
2024-06-13 23:04:49 +0300

    Gallery Details XML

--- app/src/main/java/com/example/finalproject/ui/residence_details/fragment/GalleryFragment.kt
+++ app/src/main/java/com/example/finalproject/ui/residence_details/fragment/GalleryFragment.kt
@@ -5,13 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.finalproject.R
+import com.example.finalproject.storage.BaseActivity
+import com.example.finalproject.ui.residence_details.adapter.GalleryAdapter
 
 class GalleryFragment : Fragment() {
 
+    private lateinit var baseActivity: BaseActivity
+
+    private lateinit var recyclerView: RecyclerView
+
+    private lateinit var galleryAdapter: GalleryAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        baseActivity = BaseActivity()
     }
 
     override fun onCreateView(
@@ -19,6 +30,17 @@ class GalleryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_gallery, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = view.findViewById(R.id.recycle_gallery_details)
+        galleryAdapter = GalleryAdapter()
+        recyclerView.layoutManager = GridLayoutManager(requireContext() , 2)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = galleryAdapter
+
     }
 
 }
