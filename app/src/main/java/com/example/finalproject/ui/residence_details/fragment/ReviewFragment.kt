f9f27e55a01b1c8b4138a9621e854e6b223bfd7c
AhmedSommat <ahmed.sommat97@gmail.com>
2024-06-14 04:02:13 +0300

    Review Details XML

--- app/src/main/java/com/example/finalproject/ui/residence_details/fragment/ReviewFragment.kt
+++ app/src/main/java/com/example/finalproject/ui/residence_details/fragment/ReviewFragment.kt
@@ -5,13 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.finalproject.R
+import com.example.finalproject.storage.BaseActivity
+import com.example.finalproject.ui.residence_details.adapter.GalleryAdapter
+import com.example.finalproject.ui.residence_details.adapter.ReviewAdapter
 
 class ReviewFragment : Fragment() {
 
+    private lateinit var baseActivity: BaseActivity
+
+    private lateinit var recyclerView: RecyclerView
+
+    private lateinit var reviewAdapter: ReviewAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        baseActivity = BaseActivity()
     }
 
     override fun onCreateView(
@@ -19,6 +31,17 @@ class ReviewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_review, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView = view.findViewById(R.id.recycle_review_details)
+        reviewAdapter = ReviewAdapter()
+        recyclerView.layoutManager = LinearLayoutManager(requireContext() ,
+            LinearLayoutManager.VERTICAL , false)
+        recyclerView.setHasFixedSize(true)
+        recyclerView.adapter = reviewAdapter
     }
 
 }
