cb11018c1afa6801be8500c3a3cf522682f92aee
moelkady27 <mohamedkhaledelkady11@gmail.com>
2024-06-21 00:30:01 +0300

    predicted price in adding residence

--- app/src/main/java/com/example/finalproject/ui/add_listing/activities/AddListingPredictionActivity.kt
+++ app/src/main/java/com/example/finalproject/ui/add_listing/activities/AddListingPredictionActivity.kt
@@ -4,20 +4,66 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
 import com.example.finalproject.R
+import com.example.finalproject.retrofit.RetrofitClient
+import com.example.finalproject.storage.AppReferences
 import com.example.finalproject.ui.MainActivity
+import com.example.finalproject.ui.residence_details.factory.PredictPriceFactory
+import com.example.finalproject.ui.residence_details.repository.PredictPriceRepository
+import com.example.finalproject.ui.residence_details.viewModel.PredictPriceViewModel
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlinx.android.synthetic.main.activity_add_listing_prediction.change_price_predicted_price
 import kotlinx.android.synthetic.main.activity_add_listing_prediction.finish_predicted_price
+import kotlinx.android.synthetic.main.activity_add_listing_prediction.tv_predicted_price_2
+import org.json.JSONException
+import org.json.JSONObject
 
 class AddListingPredictionActivity : AppCompatActivity() {
+
+    private lateinit var predictPriceViewModel: PredictPriceViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_listing_prediction)
 
+        initView()
+
         initButtons()
+    }
+
+    private fun initView() {
+        val predictPriceRepository = PredictPriceRepository(RetrofitClient.instance)
+        val predictPriceFactory = PredictPriceFactory(predictPriceRepository)
+        predictPriceViewModel = ViewModelProvider(this, predictPriceFactory
+        )[PredictPriceViewModel::class.java]
+
+        val token = AppReferences.getToken(this@AddListingPredictionActivity)
+        val residenceId = intent.getStringExtra("residenceId").toString()
+
+        predictPriceViewModel.predictPrice(token, residenceId)
+
+        predictPriceViewModel.predictPriceResponseLiveData.observe(this) { response ->
+            response?.let {
+                val predictedPrice = it.predicted_price.toString()
+                tv_predicted_price_2.text = predictedPrice
+            }
+        }
+
+        predictPriceViewModel.errorLiveData.observe(this) { error ->
+            error?.let {
+                try {
+                    val errorMessage = JSONObject(it).getString("message")
+                    Toast.makeText(this, errorMessage, Toast.LENGTH_LONG).show()
+                } catch (e: JSONException) {
+                    Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+
     }
 
     private fun initButtons() {
