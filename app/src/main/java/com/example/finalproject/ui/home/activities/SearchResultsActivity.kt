cc0e4de7a64d68c9c38ef7153ea475270272affd
AhmedSommat <ahmed.sommat97@gmail.com>
2024-06-21 07:03:33 +0300

    Get All Estates In Search Results

--- app/src/main/java/com/example/finalproject/ui/home/activities/SearchResultsActivity.kt
+++ app/src/main/java/com/example/finalproject/ui/home/activities/SearchResultsActivity.kt
@@ -1,33 +1,206 @@
 package com.example.finalproject.ui.home.activities
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.finalproject.R
+import com.example.finalproject.network.NetworkUtils
+import com.example.finalproject.retrofit.RetrofitClient
+import com.example.finalproject.storage.AppReferences
+import com.example.finalproject.storage.BaseActivity
+import com.example.finalproject.ui.favourite.factory.AddToFavouritesFactory
+import com.example.finalproject.ui.favourite.factory.DeleteFavouriteFactory
+import com.example.finalproject.ui.favourite.repository.AddToFavouritesRepository
+import com.example.finalproject.ui.favourite.repository.DeleteFavouriteRepository
+import com.example.finalproject.ui.favourite.viewModel.AddToFavouritesViewModel
+import com.example.finalproject.ui.favourite.viewModel.DeleteFavouriteViewModel
 import com.example.finalproject.ui.home.adapter.SearchResultsAdapter
+import com.example.finalproject.ui.home.factory.HomeFeaturedEstatesFactory
+import com.example.finalproject.ui.home.repository.HomeFeaturedEstatesRepository
+import com.example.finalproject.ui.home.viewModel.HomeFeaturedEstatesViewModel
+import kotlinx.android.synthetic.main.activity_search_results.et_search_results
 import kotlinx.android.synthetic.main.activity_search_results.toolbar_search_results
+import kotlinx.android.synthetic.main.activity_search_results.tv_search_results_title_1
+import org.json.JSONException
+import org.json.JSONObject
 
-class SearchResultsActivity : AppCompatActivity() {
+class SearchResultsActivity : BaseActivity() {
+
+    private lateinit var networkUtils: NetworkUtils
 
     private lateinit var recyclerView: RecyclerView
 
     private lateinit var searchResultsAdapter: SearchResultsAdapter
 
+    private lateinit var homeFeaturedEstatesViewModel: HomeFeaturedEstatesViewModel
+
+    private lateinit var addToFavouritesViewModel: AddToFavouritesViewModel
+
+    private lateinit var deleteFavouriteViewModel: DeleteFavouriteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_results)
 
+        networkUtils = NetworkUtils(this@SearchResultsActivity)
+
+        initRecyclerView()
+
+        initView()
+
+        initSearch()
+
+        setUpActionBar()
+    }
+
+    private fun initView() {
+                                    /* Get-All */
+
+        val homeFeaturedEstatesRepository = HomeFeaturedEstatesRepository(RetrofitClient.instance)
+        val factory = HomeFeaturedEstatesFactory(homeFeaturedEstatesRepository)
+        homeFeaturedEstatesViewModel = ViewModelProvider(
+            this@SearchResultsActivity, factory
+        )[HomeFeaturedEstatesViewModel::class.java]
+
+        getAllEstates()
+
+                                /* Add-To-Favourite */
+
+        val addToFavouritesRepository = AddToFavouritesRepository(RetrofitClient.instance)
+        val addToFavouritesFactory = AddToFavouritesFactory(addToFavouritesRepository)
+        addToFavouritesViewModel = ViewModelProvider(
+            this@SearchResultsActivity, addToFavouritesFactory
+        )[AddToFavouritesViewModel::class.java]
+
+                                /* Delete-Favourite */
+
+        val deleteFavouritesRepository = DeleteFavouriteRepository(RetrofitClient.instance)
+        val deleteFavouriteFactory = DeleteFavouriteFactory(deleteFavouritesRepository)
+        deleteFavouriteViewModel = ViewModelProvider(
+            this@SearchResultsActivity, deleteFavouriteFactory
+        )[DeleteFavouriteViewModel::class.java]
+    }
+
+    private fun getAllEstates() {
+        val token = AppReferences.getToken(this@SearchResultsActivity)
+
+        homeFeaturedEstatesViewModel.getFeaturedEstatesViewAll(token)
+
+        homeFeaturedEstatesViewModel.homeFeaturedEstatesLiveData.observe(this@SearchResultsActivity
+        ) { response ->
+            hideProgressDialog()
+            response?.let {
+                searchResultsAdapter.addItems(it.residences)
+                Log.e("FeaturedEstates", it.residences.size.toString())
+            }
+        }
+
+        homeFeaturedEstatesViewModel.errorLiveData.observe(this@SearchResultsActivity) { error ->
+            hideProgressDialog()
+            error.let {
+                try {
+                    val errorMessage = JSONObject(error).getString("message")
+                    Toast.makeText(this@SearchResultsActivity, errorMessage, Toast.LENGTH_LONG)
+                        .show()
+                } catch (_: JSONException) { }
+            }
+        }
+
+        initPagination(token)
+    }
+
+    private fun initPagination(token: String) {
+        recyclerView.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+
+                val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+                val totalItemCount = layoutManager.itemCount
+                val lastVisibleItemPosition = layoutManager.findLastVisibleItemPosition()
+
+                if (lastVisibleItemPosition + 1 >= totalItemCount) {
+                    homeFeaturedEstatesViewModel.getFeaturedEstatesViewAll(token)
+                }
+            }
+        })
+    }
+
+    private fun handleFavouriteClick(residenceId: String, isLiked: Boolean) {
+        if (!isLiked) {
+            val token = AppReferences.getToken(this@SearchResultsActivity)
+            addToFavouritesViewModel.addToFavourites(token, residenceId)
+
+            addToFavouritesViewModel.addToFavouritesLiveData.observe(
+                this@SearchResultsActivity) { response ->
+                hideProgressDialog()
+                response?.let {
+                    val status = it.status
+                    Log.e("AddToFavourites", "Added to Favourites $status")
+                    searchResultsAdapter.updateFavouriteStatus(residenceId, true)
+                }
+            }
+
+            addToFavouritesViewModel.errorLiveData.observe(this@SearchResultsActivity) { error ->
+                hideProgressDialog()
+                error.let {
+                    try {
+                        val errorMessage = JSONObject(error).getString("message")
+                        Toast.makeText(this@SearchResultsActivity, errorMessage, Toast.LENGTH_LONG)
+                            .show()
+                    } catch (_: JSONException) { }
+                }
+            }
+        } else {
+            val token = AppReferences.getToken(this@SearchResultsActivity)
+            deleteFavouriteViewModel.deleteFavourite(token, residenceId)
+
+            deleteFavouriteViewModel.deleteFavouriteLiveData.observe(this@SearchResultsActivity) { response ->
+                hideProgressDialog()
+                response?.let {
+                    val status = it.status
+                    Log.e("DeleteFavourite", "Deleted from Favourites $status")
+                    searchResultsAdapter.updateFavouriteStatus(residenceId, false)
+                }
+            }
+
+            deleteFavouriteViewModel.errorLiveData.observe(this@SearchResultsActivity) { error ->
+                hideProgressDialog()
+                error.let {
+                    try {
+                        val errorMessage = JSONObject(error).getString("message")
+                        Toast.makeText(this@SearchResultsActivity, errorMessage, Toast.LENGTH_LONG).show()
+                    } catch (_: JSONException) { }
+                }
+            }
+        }
+    }
+
+    private fun initSearch() {
+        et_search_results.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {
+                searchResultsAdapter.filterList(s.toString())
+            }
+        })
+    }
+
+    private fun initRecyclerView() {
         recyclerView = findViewById(R.id.recycle_search_results)
-
-        searchResultsAdapter = SearchResultsAdapter()
-
         recyclerView.layoutManager = LinearLayoutManager(this@SearchResultsActivity ,
             LinearLayoutManager.VERTICAL , false)
         recyclerView.setHasFixedSize(true)
+        searchResultsAdapter = SearchResultsAdapter(
+            this@SearchResultsActivity, mutableListOf(), ::handleFavouriteClick
+        )
         recyclerView.adapter = searchResultsAdapter
-
-        setUpActionBar()
     }
 
     private fun setUpActionBar() {
--- app/src/main/java/com/example/finalproject/ui/home/adapter/SearchResultsAdapter.kt
+++ app/src/main/java/com/example/finalproject/ui/home/adapter/SearchResultsAdapter.kt
@@ -1,12 +1,34 @@
 package com.example.finalproject.ui.home.adapter
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.finalproject.R
+import com.example.finalproject.ui.home.models.Residence
+import com.example.finalproject.ui.residence_details.activities.ResidenceDetailsActivity
+import kotlinx.android.synthetic.main.each_row_search_results.view.apartment_location_search_results
+import kotlinx.android.synthetic.main.each_row_search_results.view.apartment_name_search_results
+import kotlinx.android.synthetic.main.each_row_search_results.view.apartment_price_search_results
+import kotlinx.android.synthetic.main.each_row_search_results.view.image_search_results
+import kotlinx.android.synthetic.main.each_row_search_results.view.iv_search_results_fav
+import kotlinx.android.synthetic.main.each_row_search_results.view.number_star_search_results
+import kotlinx.android.synthetic.main.each_row_search_results.view.tv_apartment_search_results
+import kotlinx.android.synthetic.main.each_row_search_results.view.tv_search_results_3
+import java.util.Locale
 
-class SearchResultsAdapter: RecyclerView.Adapter<SearchResultsAdapter.MyViewHolder>() {
+class SearchResultsAdapter(
+    private val context: Context,
+    private val list: MutableList<Residence>,
+    private val onFavouriteClick: (String, Boolean) -> Unit
+): RecyclerView.Adapter<SearchResultsAdapter.MyViewHolder>() {
+
+    private var filteredList: MutableList<Residence> = list
 
     class MyViewHolder(view: View) : RecyclerView.ViewHolder(view)
 
@@ -20,10 +42,73 @@ class SearchResultsAdapter: RecyclerView.Adapter<SearchResultsAdapter.MyViewHold
     }
 
     override fun getItemCount(): Int {
-        return 20
+        return filteredList.size
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val searchResults = filteredList[position]
 
+        if (searchResults.images.isNotEmpty()) {
+            Glide.with(holder.itemView)
+                .load(searchResults.images[0].url)
+                .into(holder.itemView.image_search_results)
+        } else {
+            Glide.with(holder.itemView)
+                .load(R.drawable.home_image)
+                .into(holder.itemView.image_search_results)
+        }
+
+        holder.itemView.number_star_search_results.text = searchResults.avgRating.toString()
+        holder.itemView.tv_apartment_search_results.text = searchResults.category
+        holder.itemView.apartment_name_search_results.text = searchResults.title
+        holder.itemView.apartment_location_search_results.text = searchResults.location.fullAddress
+        holder.itemView.apartment_price_search_results.text = searchResults.salePrice.toString()
+        holder.itemView.tv_search_results_3.text = searchResults.paymentPeriod
+
+        val isLiked = searchResults.isLiked
+
+        if (isLiked) {
+            holder.itemView.iv_search_results_fav.backgroundTintList =
+                ContextCompat.getColorStateList(context, R.color.colorPrimary)
+        } else {
+            holder.itemView.iv_search_results_fav.backgroundTintList =
+                ContextCompat.getColorStateList(context, R.color.edit_text)
+        }
+
+        holder.itemView.iv_search_results_fav.setOnClickListener {
+            onFavouriteClick(searchResults._id, searchResults.isLiked)
+        }
+
+        holder.itemView.setOnClickListener {
+            val intent = Intent(holder.itemView.context, ResidenceDetailsActivity::class.java)
+            intent.putExtra("residenceId", searchResults._id)
+            intent.putExtra("residence_Id", searchResults.Id)
+            holder.itemView.context.startActivity(intent)
+        }
+    }
+
+    fun addItems(newItems: List<Residence>) {
+        val startPosition = list.size
+        list.addAll(newItems)
+        notifyItemRangeInserted(startPosition, newItems.size)
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun updateFavouriteStatus(residenceId: String, isLiked: Boolean) {
+        list.find { it._id == residenceId }?.isLiked = isLiked
+        notifyDataSetChanged()
+    }
+
+    @SuppressLint("NotifyDataSetChanged")
+    fun filterList(query: String) {
+        filteredList = if (query.trim().isBlank()) {
+            list
+        } else {
+            list.filter { featured ->
+                featured.title.lowercase(Locale.ROOT)
+                    .contains(query.trim().lowercase(Locale.ROOT))
+            }.toMutableList()
+        }
+        notifyDataSetChanged()
     }
 }
